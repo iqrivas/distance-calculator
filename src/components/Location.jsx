c6b0233e6056281d22e66da2c3d7e30f440f0088
Denisse Rivas <iqrivas@gmail.com>
2021-05-09 15:24:07 -0500

    Added show Latitude and Longitude

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,14 +3,16 @@ import '../sass/components/_Location.scss'
 
 function Location (props) {
     return (
-        <section className="location">
+        <section id={props.id} className="location">
           <h2 className= "location__title">{props.title}</h2>
           <input
               className= "location__input"  
               type="text"
               placeholder={props.placeholder}
-              id={props.id}
+              id={`autocomplete-${props.id}`}
           />
+          <p id={`${props.id}_lat`}  className= "location__details">Latitude: </p>
+          <p id={`${props.id}_lng`} className= "location__details">Longitude: </p>
         </section>
     );
 }
