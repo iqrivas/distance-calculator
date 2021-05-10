7590a5b3e5e7346618c1f5c39f913456ccb4a781
Denisse Rivas <iqrivas@gmail.com>
2021-05-10 00:59:39 -0500

    Added getting distance and duration information

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,13 +3,20 @@ import '../sass/components/_Results.scss'
 
 function Results (props) {
     return (
-        <section id="results" className="results">
+        <section id="results" className="results hidden">
           <h2 className= "results__title">Distance between Locations</h2>
-          <p id="km"  className= "results__details">KM: </p>
-          <p className= "results__details results__details--transit">Transit Time</p>
-          <p id="drive" className= "results__details">Driving: </p>
-          <p id="bike" className= "results__details">Bycicle: </p>
-          <p id="walk" className= "results__details">Walking: </p>
+          <p id="no_roads" className= "results__details results__details--warning"></p>
+          <div id="results_panel" className="hidden">
+            <p className= "results__details results__details--transit">Driving</p>
+            <p id="drive_km" className= "results__details">Distance: </p>
+            <p id="drive_hr" className= "results__details">Duration: </p>
+            <p className= "results__details results__details--transit">Bicycling</p>
+            <p id="bike_km" className= "results__details">Distance: </p>
+            <p id="bike_hr" className= "results__details">Duration: </p>
+            <p className= "results__details results__details--transit">Walking</p>
+            <p id="walk_km" className= "results__details">Distance: </p>
+            <p id="walk_hr" className= "results__details">Duration: </p>
+          </div>
         </section>
     );
 }
